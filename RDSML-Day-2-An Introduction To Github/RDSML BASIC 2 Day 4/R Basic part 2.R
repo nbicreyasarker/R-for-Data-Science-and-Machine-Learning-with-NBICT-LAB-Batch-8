88d0d6037551b6262deec76ea2f39b3bbfbb734f
nbicreyasarker <reyas0480@gmail.com>
2025-05-24 13:43:27 +0600

    DAY 2 Practice

---
+++ RDSML-Day-2-An Introduction To Github/RDSML BASIC 2 Day 4/R Basic part 2.R
@@ -0,0 +1,47 @@
+# Data Types in R
+# 1.Numeric -Integer / Floating point
+# 2.String -"Reya Sarker"/ 'Reya Sarker'
+# 3.Logical 
+
+a = 11
+b = 7.19
+class(a)
+class(b)
+
+name ="Reya Sarker"
+print(name)
+class(name)
+
+c = "52"
+print(c+9)
+print(a+9)
+class(c)
+
+#Logical Data Type - TRUE , FALSE (T , F)
+print(a < b)
+x = 19
+y = 22-3
+print(x==y)
+print(x !=y)
+print(y>=x)
+
+# logical Operators In R
+# && Logical Operator
+# || Logical operator
+
+4 == 4 && 5 == 5
+x !=y || x==y
+
+# Simple Mathematical Operations In R
+z=6+9-8*(2/3)  
+print(z)
+
+w=(4+3-3)*2/3
+print(w)
+
+y=4^3
+print(y)
+
+11/5
+11%%5
+11%/%5
--- RDSML-Day-2-An Introduction To Github/first.R
+++ RDSML-Day-2-An Introduction To Github/first.R
@@ -3,3 +3,10 @@ data = c(9,6,17,31,11)
 x = mean(data)
 summary(data)
 z = x/2
+
+
+
+
+
+
+
